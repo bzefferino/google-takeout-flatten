2576e20e2d758b90160b208dbf843bbb35d3edb8
Benito Zefferino <42010637+bzefferino@users.noreply.github.com>
2023-06-18 21:00:02 -0600

    create dest dir if not exists

--- takeout.ps1
+++ takeout.ps1
@@ -6,10 +6,15 @@ param (
     [string]$DestinationDirectory
 )
 
-$fileList = Get-ChildItem -Recurse -File -Include *.jpg,*.jpeg,*.mp4,*.PNG,*.gif,*.MP -LiteralPath $SourceDirectory
+$fileList = Get-ChildItem -Recurse -File -LiteralPath $SourceDirectory
 
 $totalFiles = $fileList.Count
 $currentFile = 0
+
+# Check if the destination directory exists, and create it if necessary
+if (-not (Test-Path -Path $DestinationDirectory)) {
+    New-Item -ItemType Directory -Path $DestinationDirectory | Out-Null
+}
 
 foreach ($file in $fileList) {
     $currentFile++
@@ -17,9 +22,9 @@ foreach ($file in $fileList) {
 
     Write-Progress -Activity "Moving Files" -Status "Progress: $progressPercentage% ($currentFile/$totalFiles)" -PercentComplete $progressPercentage
 
-	if ($file.Name -notlike "*.json") {
-		Move-Item -Path $file.FullName -Destination $DestinationDirectory -Force
-	}    
+    if ($file.Name -notlike "*.json") {
+        Move-Item -Path $file.FullName -Destination $DestinationDirectory -Force
+    }    
 }
 
 Write-Progress -Activity "Moving Files" -Status "Completed" -Completed
